9481c0a040784b206b2583c6ea476d0f2e35163c
Sam <svtagalog@joyride.com.ph>
2023-04-16 03:13:13 +0800

    chore: try test token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -23,8 +23,7 @@ const edgeFunction = api.create({
 
 axios.interceptors.request.use(
   (config) => {
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzbnJ3bW1vbGNiaGduY3dvZ294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ3MzM1MTAsImV4cCI6MTk5MDMwOTUxMH0.Vgv78ZxFTJQ1Dl7pCn352dE_TfE_cveRLqEB7pa_w4s"; //test token
+    const token = ""; //test token
     config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
